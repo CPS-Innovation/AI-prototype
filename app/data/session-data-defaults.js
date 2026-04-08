69677882868561e94409a31b729c5b8edd2337f1
Lydia Teebay <lydia.teebay@cps.gov.uk>
2026-04-08 10:04:22 +0100

    adding in materials and notifications

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,9 @@
 module.exports = {
 
-  // Insert values here
+  // Materials card notification state: 'complete' | 'incomplete'
+  'm-status': 'complete',
+
+  // Whether new content has been added since last review: 'true' | 'false'
+  'm-new': 'false'
 
 }
